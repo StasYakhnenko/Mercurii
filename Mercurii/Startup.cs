19e76b75cee06481ef4328b0107da2a513d33fe4
StasYakhnenko <ghosteagle07@gmail.com>
2017-01-19 13:37:13 +0200

    Добавьте файлы проекта.

---
+++ Mercurii/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Mercurii.Startup))]
+namespace Mercurii
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
